369bb4e5674d1d7713982af88289b84f35d6d2f9
Errnie <38852743+Errnie@users.noreply.github.com>
2018-04-30 09:42:39 +0100

    calculator using function

---
+++ function.py
@@ -0,0 +1,14 @@
+
+y = int(input("input the first no  "))
+z = int(input("input the second no  "))       
+def mult (y,z):
+    mult = (y*z)
+    print (mult)
+
+mult (z,y)
+
+def div (y,z):
+    div = (y/z)
+    print (div)
+
+div (y,z)
